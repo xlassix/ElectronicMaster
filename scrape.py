e65852bc4eb81c9642d951b30625abff44e7faad
xlassix <46370698+xlassix@users.noreply.github.com>
2021-12-22 04:28:01 +0100

    Scraper

--- scrape.py
+++ scrape.py
@@ -11,32 +11,7 @@ _output_dir = "output"
 makedirs(_output_dir, exist_ok=True)
 
 
-def getTextById(browser: webdriver, _id: str) -> str:
-    """This Function Gets Text from the WebDriver Instance that matches the HTML AttributeId
-
-    Args:
-        browser (webdriver): Selenium.WebDriver,
-        _id(str): id HTMLAttribute
-
-    Returns:
-        str: String that matches the query
-    """
-    return browser.find_element(by=By.ID, value=_id).text.replace(",", "")
-
-
-def getTextByXPath(browser: webdriver, xpath: str) -> str:
-    """This Function Gets Text from the WebDriver Instance that matches the Xpath
-
-    Args:
-        browser (webdriver): Selenium.WebDriver,
-        _id(str): id HTMLAttribute
-
-    Returns:
-        str: String that matches the query
-    """
-    return browser.find_element(by=By.XPATH, value=xpath).text.replace(",", "")
-
-def parseDate(dateStr: str,dateFormat:str="%m/%d/%y") -> date:
+def parseDate(dateStr: str, dateFormat: str = "%m/%d/%y") -> date:
     """This function takes a date string in the format `dateFormat` and returns a datetime object
 
     Args:
@@ -59,8 +34,66 @@ def parseFloat(_number: str) -> float:
     """
     return float(_number.strip().strip("+").strip("$"))
 
+
+def getExcels(path: str) -> [str]:
+    """This function returns the list of excel in path
+
+    Args:
+        path (str)
+
+    Returns:
+        [str]: list of excel(.xlsx) in path
+    """
+    return (list(filter(lambda elem: elem.endswith(".csv") or elem.endswith(".xlsx"), listdir(path))))
+
+
 class Scraper():
-    def __init__(self):
-        browser = webdriver.Chrome(
-        service=Service(ChromeDriverManager().install()))
+    def __init__(self,source:str):
+        assert source.lower() in ['masterelectronics.com','mini-circuits.com']
+        self._browser = webdriver.Chrome(
+            service=Service(ChromeDriverManager().install()))
+        self._source = source
+        
+    def getTextById(self, _id: str) -> str:
+        """This Function Gets Text from the WebDriver Instance that matches the HTML AttributeId
+
+        Args:
+            _id(str): id HTMLAttribute
+
+        Returns:
+            str: String that matches the query
+        """
+        return self._browser.find_element(by=By.ID, value=_id).text.replace(",", "")
+
+
+    def getTextByXPath(self, xpath: str) -> str:
+        """This Function Gets Text from the WebDriver Instance that matches the Xpath
+
+        Args:
+            _id(str): id HTMLAttribute
+
+        Returns:
+            str: String that matches the query
+        """
+        return self._browser.find_element(by=By.XPATH, value=xpath).text.replace(",", "")
+
+    def getItem( item: str) -> bool:
+        """This Function Checks if an item query as any results on the current page
+            it returns true to indicate if on the right page and false if the search item returns no results
+
+        Args:
+            item (str): Query
+
+        Returns:
+            bool
+        """
+        url = (
+            "https://www.masterelectronics.com/en/keywordsearch?text={0}".format(item))
+        self._browser.get(url)
+        if(self._browser.current_url == url):
+            self._browser.find_element(by=By.XPATH,
+                                value='//*[@id="search-content-results"]/div/div[2]/a[1]').click()
+        elif("https://www.masterelectronics.com/en/requestfornotifications" in self._browser.current_url):
+            return False
+        return True
 
